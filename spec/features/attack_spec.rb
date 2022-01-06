25c3d702c8a8c2673208169a0ab8e347f2907948
icvotria <icvotria@gmail.com>
2022-01-06 22:53:33 +0000

    decorated attack view

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'Attack' do
   scenario 'attack player 2' do
     sign_in_and_play
-    click_link 'Attack!'
+    click_on 'Attack!'
     expect(page).to have_content 'Jones has been hit!'
   end
 end

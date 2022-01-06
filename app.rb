c8f0159283cf116ba2d2362d4e045bfbba2cc004
icvotria <icvotria@gmail.com>
2022-01-06 22:33:04 +0000

    attack

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1_name]
-    session[:player_2] = params[:player_2_name]
+    session[:player_1] = params[:player_1_name].capitalize
+    session[:player_2] = params[:player_2_name].capitalize
     redirect '/play'
   end
 
@@ -22,6 +22,12 @@ class Battle < Sinatra::Base
     @player_1_name = session[:player_1]
     @player_2_name = session[:player_2]
     erb :play
+  end
+
+  get '/attack' do
+    @player_1_name = session[:player_1]
+    @player_2_name = session[:player_2]
+    erb :attack
   end
 
   run! if app_file == $0

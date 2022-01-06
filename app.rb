81ede3c969b6f1a5c0186dddbeb06af9b0867b04
icvotria <icvotria@gmail.com>
2022-01-06 23:23:31 +0000

    swapped to global variables

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'player'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -13,20 +14,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1_name].capitalize
-    session[:player_2] = params[:player_2_name].capitalize
+    # I promise to never use global variables in tech tests
+    $player_1 = Player.new(params[:player_1_name].capitalize)
+    $player_2 = Player.new(params[:player_2_name].capitalize)
+    # session[:player_1] = params[:player_1_name].capitalize
+    # session[:player_2] = params[:player_2_name].capitalize
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1]
-    @player_2_name = session[:player_2]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    # @player_1_name = session[:player_1]
+    # @player_2_name = session[:player_2]
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1]
-    @player_2_name = session[:player_2]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    # @player_1_name = session[:player_1]
+    # @player_2_name = session[:player_2]
     erb :attack
   end
 
